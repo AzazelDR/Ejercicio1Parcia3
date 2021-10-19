baa251204fc3ffbfd37f78f0bdb3e690689e0e82
AzazelDR - Axel Reyes <49660378+AzazelDR@users.noreply.github.com>
2021-10-18 19:12:47 -0600

    Parcial

---
+++ js/Ejercicio1.js
@@ -0,0 +1,5 @@
+var A = parseFloat(prompt("Ingrese el valor A --> 5"));
+var B = parseFloat(prompt("Ingrese el valor B --> 3"));
+
+var Resultado = ((A + 5) * (B / 3) + Math.pow(Math.pow((A * B), 2), 3));
+alert("El Resultado es " + Resultado);
